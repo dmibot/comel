c1fef1e924c53dd9e2247d4269799890394629a1
kerreeehoreee <99607873+dmibot@users.noreply.github.com>
2022-04-18 01:52:15 +0700

    comel

--- command/other/kata.js
+++ command/other/kata.js
@@ -21,7 +21,7 @@ module.exports = {
                 }]
                 await sock.sendMessage(msg.from, {
                     text: "Silahkan Pilih Melalui Tombol Dibawah.",
-                    footer: "Kaguya PublicBot • FaizBastomi",
+                    footer: "Ayra si Comel • OpuxTad",
                     title: "Random Kata",
                     buttonText: "List",
                     sections: secs
